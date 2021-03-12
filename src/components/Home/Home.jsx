a6a29086a3c56547c33a25da5bc6b24db491aa06
fernandaturbay <fernanda.turbay@gmail.com>
2021-03-11 23:06:48 -0300

    Subindo 'Bons Drinks'

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './Home.css'
+
+class Home extends Component {
+    render() {
+        return (
+            <main className = 'main'>
+                <h1>Bons Drinks</h1>
+            </main>
+                
+        )
+    }
+}
+
+export default Home
