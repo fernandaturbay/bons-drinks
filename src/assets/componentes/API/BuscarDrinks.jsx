d77682b2b97b0c0cbe58f0225b1108405996cc86
fernandaturbay <fernanda.turbay@gmail.com>
2021-03-27 22:45:30 -0300

    "Primeira parte da requisição"

--- src/assets/componentes/API/BuscarDrinks.jsx
+++ src/assets/componentes/API/BuscarDrinks.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import Botao from '../Button/Button'
 import "./estiloDrinks.css"
+import {requisicao} from "../../../model/arquivoFetch"
+import {useState, useEffect} from 'react'
 
 export default function BuscarDrinks() {
+    const [dadosApi, setDadosApi] = useState("");
+    
+    useEffect(async () => {
+        const req = await requisicao();
+        setDadosApi(req);
+        console.log(req)
+      }, []);
+
+    const arrMap = [1, 2, 3, 4, 5, 6, 7]
+
     return (
         <div className = "gridApi">
             <section className= "cabecalho">
@@ -11,12 +23,18 @@ export default function BuscarDrinks() {
                     <h1>DRINKS</h1>
                 </div>
                 <div className="positionBotao">
-                    <Botao titulo="Drinks Populares"/>
-                    <Botao titulo="Buscar Drinks"/>
+                    <Botao estilo = "botao" titulo="Drinks Populares"/>
+                    <Botao estilo = "botao" titulo="Buscar Drinks"/>
                 </div>
             </section>
             <section className="conteudoDrink">
-
+                {/* {arrMap.map(()
+                    (valor, index)=>{
+                        <>
+                        <h1>{dadosApi[]}</h1>
+                        <p></p>
+                    }
+                ) */}
             </section>
         </div>
     )
--- src/model/arquivoFetch.js
+++ src/model/arquivoFetch.js
@@ -1,9 +1,9 @@
-export async function request() {
-    const response = await fetch (
-        "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"mm
+export async function requisicao() {
+    const response = await fetch(
+      "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
     );
-
-    const dados = await response.json ();
+  
+    const dados = await response.json();
 
     return dados.drinks;
-};
+  };
